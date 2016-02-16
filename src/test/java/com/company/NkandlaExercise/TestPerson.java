e12fb991f372dbd04b69ff28a7d281b729b7546f
Iddo-Imri <iddo.imri@gmail.com>
2016-02-16 14:35:57 +0200

    Persons Test

---
+++ src/test/java/com/company/NkandlaExercise/TestPerson.java
@@ -0,0 +1,46 @@
+package com.company.NkandlaExercise;
+
+import junit.framework.TestCase;
+public class TestPerson extends TestCase {
+	
+JacobZuma Jz;
+Person Hz;
+Person Jm;
+
+
+public void testJz () {
+	assertNotNull(Jz.getArchitect());
+	assertNotNull(Jz.getLawyer());
+	
+	assertTrue(Jz.getName().compareTo("Jacob Zuma") == 0);
+	assertTrue(Jz.getGender().compareTo("M") == 0);
+	assertTrue(Jz.getType() == PersonType.Politician);
+	assertTrue (Jz.getAge() >= 20);
+	assertTrue (Jz.getAge() <= 100);
+	}
+
+@Override
+protected void setUp() throws Exception {
+	// TODO Auto-generated method stub
+	super.setUp();
+	Jz = new JacobZuma("Jacob Zuma", 55, "M", PersonType.Politician, new Architect("John", 21, "M", PersonType.Architect), new Lawyer("James", 25, "F", PersonType.Lawyer));
+	Hz = new HelenZille("Helen Zille", 55, "F", PersonType.Politician);
+	Jm = new JuliusMalema("Julius Malema", 55, "M", PersonType.Politician);
+}
+
+public void testHz () {
+	assertTrue(Hz.getName().compareTo("Helen Zille") == 0);
+	assertTrue(Hz.getGender().compareTo("F") == 0);
+	assertTrue(Hz.getType() == PersonType.Politician);
+	assertTrue (Hz.getAge() >= 20);
+	assertTrue (Hz.getAge() <= 100);
+	}
+
+public void testJm () {
+	assertTrue(Jm.getName().compareTo("Julius Malema") == 0);
+	assertTrue(Jm.getGender().compareTo("M") == 0);
+	assertTrue(Jm.getType() == PersonType.Politician);
+	assertTrue (Jm.getAge() >= 20);
+	assertTrue (Jm.getAge() <= 100);
+	}
+}
